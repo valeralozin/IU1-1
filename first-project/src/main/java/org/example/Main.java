bffd3b7ad559a05684ad985df29f245d25812cc7
valeralozin <lozinvp@student.bmstu.ru>
2022-12-27 22:59:59 +0300

    1proj

--- first-project/src/main/java/org/example/Main.java
+++ first-project/src/main/java/org/example/Main.java
@@ -5,7 +5,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("+");
+        Scanner console =  new Scanner(System.in);
+        int[] arr;
+        arr=new int [4];
+        for (int i = 0; i<4; i++ ){
+            arr[i] = console.nextInt();
+        }
+        int m = -100000000;
+        for (int i=0;i<4;i++){
+            if (m<arr[i]){
+                m=arr[i];
+            }
+        }
+        System.out.println(m);
 
     }
 }
